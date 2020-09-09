244e4c9801571459e4461fafecf6a1d3b5c413e4
www199403 <wuwei@LAPTOP-Q2D37R13.localdomain>
2020-09-10 00:19:32 +0800

    项目迁移到复读机

--- src/main/main.c
+++ src/main/main.c
@@ -2,7 +2,23 @@
 
 int main(){
     char str[100];
+     printf("please put a string\n");
     scanf("%s",str);
     printf("hello world  ,%s\n",str);
+
+    while (1){
+        char info[255];
+        scanf ("%s",info);
+        printf("%s\n",info);
+        //todo
+        if (info[0] =='o'){
+            break;
+        }
+        int i = 0;
+        i++;
+        
+    }
+    printf("\nfinish \n");
+
     return 0;
 }
